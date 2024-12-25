34d862dc307df81d27a8c197c404b3bb81a1ddd9
Curtis Erickson <incognitus.me@gmail.com>
2024-12-25 14:07:51 -0600

    fix: Arcane Shot was incorrectly adding base range damage

--- hunter/arcane-shot.lua
+++ hunter/arcane-shot.lua
@@ -6,8 +6,8 @@ local FindTextInTooltip        = SPELL_ANALYSIS.FindTextInTooltip
 local SPELL_TREE_ID            = SPELL_ANALYSIS.SPELL_TREE_ID
 local SPELL_POWER_TYPE         = SPELL_ANALYSIS.SPELL_POWER_TYPE
 local ReverseLookupTable       = SPELL_ANALYSIS.ReverseLookupTable
-local AnalyzeDamageRangeSpell  = SPELL_ANALYSIS.AnalyzeDamageRangeSpell
-local AddDamageRangeAnalysis   = SPELL_ANALYSIS.AddDamageRangeAnalysis
+local AnalyzeFlatDamageSpell   = SPELL_ANALYSIS.AnalyzeFlatDamageSpell
+local AddDamageAnalysis        = SPELL_ANALYSIS.AddDamageAnalysis
 local AddPowerAnalysis         = SPELL_ANALYSIS.AddPowerAnalysis
 
 -- spell stuff
@@ -36,11 +36,11 @@ SPELL_ANALYSIS.FUN[SPELL_NAME] = function(tooltip)
     local cooldown = tonumber(FindTextInTooltip(tooltip, cooldownPattern))
 
     -- analyze
-    local result = AnalyzeDamageRangeSpell(empoweredLow, empoweredHigh, 0, cooldown, SPELL_TREE_ID.ARCANE,
+    local result = AnalyzeFlatDamageSpell(bonusDamage, 0, cooldown, SPELL_TREE_ID.ARCANE,
         SPELL_POWER_TYPE.MANA, cost, 0)
 
     -- add line
     tooltip:AddLine("\n")
-    AddDamageRangeAnalysis(tooltip, result)
-    AddPowerAnalysis(tooltip, { range = result })
+    AddDamageAnalysis(tooltip, result)
+    AddPowerAnalysis(tooltip, { flat = result })
 end
